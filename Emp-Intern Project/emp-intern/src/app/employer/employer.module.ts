3ba67985432fe37864b9ecfb41b51a78c54c28a2
reginaldshub <reginald.anthony@accionlabs.com>
2018-12-14 15:59:39 +0530

    emp-intern

---
+++ Emp-Intern Project/emp-intern/src/app/employer/employer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployerRoutingModule } from './employer-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmployerRoutingModule
+  ]
+})
+export class EmployerModule { }
