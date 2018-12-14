0f6997a7664e47e24c6b37d93c0fba50a0bb6727
santhosh <santhoshzcv>
2018-12-14 16:30:17 +0530

    updated

---
+++ Emp-Intern Project/emp-intern/src/app/intern/intern.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InternRoutingModule } from './intern-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InternRoutingModule
+  ]
+})
+export class InternModule { }
