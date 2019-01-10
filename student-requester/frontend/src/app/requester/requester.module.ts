445c506e3cd6c9c28e8af864c2944e44a40fb45b
reginaldshub <reginald.anthony@accionlabs.com>
2019-01-10 16:00:54 +0530

    service

--- student-requester/frontend/src/app/requester/requester.module.ts
+++ student-requester/frontend/src/app/requester/requester.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { RequesterRoutingModule } from './requester-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { EdudetailsComponent } from './edudetails/edudetails.component';
 import { ReqpermissionComponent } from './reqpermission/reqpermission.component';
 import { ReqdashboardComponent } from './reqdashboard/reqdashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MaterialModule } from '../material/material.module';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   imports: [
@@ -18,6 +18,7 @@ import { MaterialModule } from '../material/material.module';
     MaterialModule,
     ReactiveFormsModule
   ],
-  declarations: [DashboardComponent, EdudetailsComponent, ReqpermissionComponent, ReqdashboardComponent, ProfileComponent]
+  declarations: [ EdudetailsComponent, ReqpermissionComponent, ReqdashboardComponent, ProfileComponent],
+  providers: [CookieService]
 })
 export class RequesterModule { }
