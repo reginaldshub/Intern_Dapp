b17233008e89dfff923f95951102035d360d80d0
santhosh <santhoshzcv>
2019-01-11 12:14:38 +0530

    account

--- student-requester/frontend/src/app/common/account/account.component.ts
+++ student-requester/frontend/src/app/common/account/account.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ServiceService } from '../service.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+export interface Network {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-account',
@@ -6,10 +15,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  account:string;
+  networks: Network[] = [
+    {value: 'localhost', viewValue: 'Localhost 8545'},
+    {value: 'ropston', viewValue: 'Ropston'},
+    {value: 'rinkbey', viewValue: 'Rinkeby'}
+  ];
 
-  constructor() { }
+  Account: FormGroup = new FormGroup({
+    network: new FormControl('',Validators.required),
+    accountNumber:new FormControl('',Validators.required)
+  })
+
+  Create: FormGroup = new FormGroup({
+    password: new FormControl('',Validators.required)
+  })
+  
+  constructor(private service:ServiceService,private router:Router) { }
 
   ngOnInit() {
   }
+  onSubmit(){
+    console.log(this.Account.value);
+    this.service.attach(this.Account.value).subscribe((res)=>{
+      console.log(res);
+    },err=>{
+      if(err instanceof HttpErrorResponse){
+        if(err.status === 401){
+          this.router.navigate(['/login']);
+        }
+      }
+    }) 
+  }
 
+  onCreate(){
+    console.log(this.Create.value);
+    this.service.newaccount(this.Create.value).subscribe((res)=>{
+     if(res['result']){
+       this.account=res['result'];
+     }
+    },
+    err=>{
+      if(err instanceof HttpErrorResponse){
+        if(err.status === 401){
+          this.router.navigate(['/login']);
+        }
+      }
+    })
+  } 
 }
