0ef854e3640e33ee3735bd3a6503cf8c39057d69
santhosh <santhoshzcv>
2019-01-14 18:35:15 +0530

    10th frontend

---
+++ student-requester/frontend/src/app/student/education/add10th/add10th.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators,FormArray,FormBuilder } from '@angular/forms';
+@Component({
+  selector: 'app-add10th',
+  templateUrl: './add10th.component.html',
+  styleUrls: ['./add10th.component.css']
+})
+export class Add10thComponent implements OnInit {
+  constructor(private fb:FormBuilder) {
+   
+  }
+  ngOnInit() {
+ 
+  }
+  add10th: FormGroup = this.fb.group({
+    studentid: [],
+    ecategory: [],
+    Startyear: [],
+    Endyear:   [],
+    addsubjects: this.fb.array([this.addSubjectGroup()])
+  });
+
+  addSubjectGroup(){
+    return this.fb.group({
+      subjectname:[],
+      subjectmarks:[]
+    });
+  }
+
+  get subjectArray(){
+  return <FormArray> this.add10th.get('addsubjects');
+  }
+
+  add(){
+    this.subjectArray.push(this.addSubjectGroup())
+  }
+  Remove(index){
+    this.subjectArray.removeAt(index);
+  }
+  value(){
+    console.log(this.add10th.value)
+  }
+
+}
--- student-requester/frontend/src/app/student/student-routing.module.ts
+++ student-requester/frontend/src/app/student/student-routing.module.ts
@@ -5,10 +5,12 @@ import { EducationComponent } from './education/education.component';
 import { RequestsComponent } from './requests/requests.component';
 import { AuthGuard } from '../auth.guard';
 import { StudentProfileComponent } from './student-profile/student-profile.component';
+import { Add10thComponent } from './education/add10th/add10th.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'education', component: EducationComponent, canActivate: [AuthGuard] },
+  { path: 'add10th',component:Add10thComponent, canActivate:[AuthGuard]},
   { path: 'requests', component: RequestsComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: StudentProfileComponent, canActivate: [AuthGuard] },
 ];
