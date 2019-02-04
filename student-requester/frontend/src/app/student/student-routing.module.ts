a2e848ef33ada8ed964d68f8edfcdbed34dbc041
santhosh <santhoshzcv>
2019-02-04 15:12:46 +0530

    msg

--- student-requester/frontend/src/app/student/student-routing.module.ts
+++ student-requester/frontend/src/app/student/student-routing.module.ts
@@ -5,11 +5,7 @@ import { EducationComponent } from './education/education.component';
 import { RequestsComponent } from './requests/requests.component';
 import { AuthGuard } from '../auth.guard';
 import { StudentProfileComponent } from './student-profile/student-profile.component';
-import { Add10thComponent } from './education/add10th/add10th.component';
 import { AccountComponent } from './account/account.component';
-import { PucComponent } from './education/puc/puc.component';
-import { DegreeComponent } from './education/degree/degree.component';
-import { MastersComponent } from './education/masters/masters.component';
 import { SelfcertificateComponent } from './education/selfcertificate/selfcertificate.component';
 import { CommitComponent } from './education/commit/commit.component';
 
@@ -21,10 +17,6 @@ const routes: Routes = [
      { path: 'education', component: EducationComponent},
      { path: 'profile', component: StudentProfileComponent},
      { path: 'requests', component: RequestsComponent},
-     { path: 'add10th', component: Add10thComponent},
-     { path: 'puc', component: PucComponent},
-     { path: 'degree', component:DegreeComponent},
-     { path: 'masters', component:MastersComponent},
      { path: 'certificates', component:SelfcertificateComponent},
      { path: 'commit', component:CommitComponent},
      { path: 'account', component:AccountComponent}
